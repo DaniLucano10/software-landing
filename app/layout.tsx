48ae3e7194c08bcdf0d757f8186a7a3d7aece442
DaniLucano10 <olucano10@gmail.com>
2025-05-16 17:44:15 -0500

    Cambiando titulo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Rajdhani } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Provider from "@/components/HOC/Provider";
 
 const font = Rajdhani({
   weight: ["300", "400", "500", "600", "700"],
@@ -9,7 +10,7 @@ const font = Rajdhani({
 })
 
 export const metadata: Metadata = {
-  title: "Software Landing Page",
+  title: "Software | Landing",
   description: "Software Landing Page using Next.js",
   icons: {
     icon: "/favicon.ico",
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${font.className} antialiased`}
       >
-        <ResponsiveNav />
-        {children}
+        <Provider>
+          <ResponsiveNav />
+          {children}
+        </Provider>
       </body>
     </html>
   );
