fc7bda4c3e275499c05b8014a78084402e519523
DaniLucano10 <olucano10@gmail.com>
2025-05-16 17:45:13 -0500

    pagina de home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Hero from './Hero/Hero';
+import Services from './Services/Services';
+import About from './About/About';
+import WhyChoose from './WhyChoose/WhyChoose';
+import Software from './Software/Software';
+import Project from './Project/Project';
+import Review from './Review/Review';
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
-      Home
+      <Hero  />
+      <Services />
+      <About />
+      <WhyChoose />
+      <Software />
+      <Project />
+      <Review />
     </div>
   )
 }
 
-export default Home
+export default Home;
